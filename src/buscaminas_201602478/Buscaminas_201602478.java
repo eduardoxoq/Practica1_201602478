1a351f1fa70d308963eadc5bf5ec58358a966b79
Eduardo <Eduardo@192.168.0.15>
2017-08-15 23:39:29 -0600

    inicio

---
+++ src/buscaminas_201602478/Buscaminas_201602478.java
@@ -0,0 +1,35 @@
+
+package buscaminas_201602478;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Buscaminas_201602478 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String opcion1;
+        Scanner sn = new Scanner(System.in);
+        Menu objeto = new Menu();
+        
+        
+        System.out.println("             Eduardo Francisco Xoquic Poz                  ");
+        System.out.println("                    201602478                              ");
+        System.out.println("                 IPC A Practica 1                          ");
+        System.out.println("                 ¡¡¡BUSCAMINAS!!!                          ");
+        
+        opcion1 = sn.nextLine();
+       
+       if(opcion1.equals("")){
+         objeto.menuprincipal();
+           
+    }
+        
+       
+    }
+    
+}
---
+++ src/buscaminas_201602478/Menu.java
@@ -0,0 +1,28 @@
+
+package buscaminas_201602478;
+import java.util.Scanner;
+
+        /**
+ *
+ * @author Eduardo
+ */
+public class Menu {
+    public void menuprincipal(){
+        int opcion;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("...................................................");
+        System.out.println("                  ¡BUSCAMINAS!");
+        System.out.println("                1.Principiante");
+        System.out.println("                2.Intermedio");
+        System.out.println("                3.Experto");
+        System.out.println("                4.Salir");
+        System.out.println("...................................................");
+        //System.out.println("");
+        System.out.println("Ingrese opcion");
+        opcion = in.nextInt();
+    
+    }  
+    
+    
+}
